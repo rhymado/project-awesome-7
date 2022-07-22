4de336c3887c40dddf47a6303a553d35dfca0e4b
rhymaado <mightydhiyamatra@yahoo.co.id>
2022-07-22 16:10:11 +0700

    remote notif

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 import "react-native-gesture-handler";
 import React from "react";
-import {AppRegistry, Platform} from "react-native";
+import {AppRegistry} from "react-native";
 import {NavigationContainer} from "@react-navigation/native";
 import {SafeAreaProvider} from "react-native-safe-area-context";
 import PushNotification from "react-native-push-notification";
 
 import App from "./src";
 import {name as appName} from "./app.json";
+import {sendLocalNotification} from "./src/helpers/notification";
 
 // Must be outside of any component LifeCycle (such as `componentDidMount`).
 PushNotification.configure({
-  // (optional) Called when Token is generated (iOS and Android)
-  onRegister: function (token) {
-    console.log("TOKEN:", token);
+  onRegister: token => {
+    console.log("token:", token.token);
   },
 
   // (required) Called when a remote is received or opened, or local notification is opened
-  onNotification: function (notification) {
+  onNotification: notification => {
     console.log("NOTIFICATION:", notification);
+    sendLocalNotification(notification.title, notification.message);
 
     // process the notification
 
@@ -26,7 +27,7 @@ PushNotification.configure({
   },
 
   // (optional) Called when Registered Action is pressed and invokeApp is false, if true onNotification will be called (Android)
-  onAction: function (notification) {
+  onAction: notification => {
     console.log("ACTION:", notification.action);
     console.log("NOTIFICATION:", notification);
 
@@ -34,8 +35,8 @@ PushNotification.configure({
   },
 
   // (optional) Called when the user fails to register for remote notifications. Typically occurs when APNS is having issues, or the device is a simulator. (iOS)
-  onRegistrationError: function (err) {
-    console.error(err.message, err);
+  onRegistrationError: err => {
+    console.log(err.message, err);
   },
 
   // Should the initial notification be popped automatically
@@ -49,7 +50,7 @@ PushNotification.configure({
    * - if you are not using remote notification or do not have Firebase installed, use this:
    *     requestPermissions: Platform.OS === 'ios'
    */
-  requestPermissions: Platform.OS === "ios",
+  requestPermissions: true,
 });
 
 PushNotification.createChannel(
@@ -59,6 +60,8 @@ PushNotification.createChannel(
   },
   created => console.log("channel is ", created ? "created" : "available"),
 );
+// console.log("request perm");
+// PushNotification.requestPermissions();
 
 const AppWithRouter = () => (
   <SafeAreaProvider>
